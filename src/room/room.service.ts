68285e9f3d6bc5480a0d62d513b708807bed1733
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-08 20:08:15 -0300

    feat: implement prisma

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 import { CreateRoomDto } from './dto/create-room.dto';
-import { UpdateRoomDto } from './dto/update-room.dto';
 
 @Injectable()
 export class RoomService {
-  create(createRoomDto: CreateRoomDto) {
-    return 'This action adds a new room';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService,
+  ) {}
+
+  async createRoomAndUserHost(createRoomDto: CreateRoomDto) {
+    try {
+      const allNumbersDrawn = this.createAllNumbersDrawn();
+
+      const data: Prisma.RoomCreateInput = {
+        name: createRoomDto.name,
+        drawnNumber: allNumbersDrawn,
+        status: true,
+        ballTime: createRoomDto.ballTime,
+        useCards: createRoomDto.userCards,
+      };
+      const room = await this.prisma.room.create({ data });
+
+      const userHost: CreateUserDto = {
+        nickname: createRoomDto.nickname,
+        roomId: room.id,
+      };
+      const user = await this.userService.create(userHost);
+
+      return {
+        roomId: room.id,
+        userId: user.id,
+      };
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all room`;
-  }
+  createAllNumbersDrawn() {
+    const allNumbersDrawn: number[] = [];
 
-  findOne(id: number) {
-    return `This action returns a #${id} room`;
-  }
+    while (allNumbersDrawn.length < 75) {
+      let drawnNumber: number = Math.floor(Math.random() * (76 - 1)) + 1;
 
-  update(id: number, updateRoomDto: UpdateRoomDto) {
-    return `This action updates a #${id} room`;
-  }
+      if (!allNumbersDrawn.includes(drawnNumber)) {
+        allNumbersDrawn.push(drawnNumber);
+      }
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} room`;
+    return allNumbersDrawn;
   }
 }
