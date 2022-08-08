a9ed9e0c672489285596b4a33601f35c8a7d6ad5
leotinoco7 <leofptinoco7@gmail.com>
2022-08-08 20:45:02 -0300

    is-admin: adding admin only function

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -10,7 +10,7 @@ import {
 import { RoomService } from './room.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('room')
 @Controller('room')
@@ -20,5 +20,21 @@ export class RoomController {
   @Post()
   createRoomAndUserHost(@Body() createRoomDto: CreateRoomDto) {
     return this.roomService.createRoomAndUserHost(createRoomDto);
+  }
+
+  @Get(':roomId')
+  @ApiOperation({
+    summary: 'Get a room by its ID',
+  })
+  findOne(@Param('id') id: string) {
+    return this.roomService.findOne(id);
+  }
+
+  @Get(':roomId')
+  @ApiOperation({
+    summary: 'Get all running rooms',
+  })
+  findAll(@Param('id') id: string) {
+    return this.roomService.findOne(id);
   }
 }
