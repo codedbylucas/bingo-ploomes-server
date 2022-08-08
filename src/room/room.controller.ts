137da76c320787e3b62cdd86bd16fdc9eb2846c0
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-08 20:13:39 -0300

    fix: user service and room controller

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -10,27 +10,7 @@ export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
   @Post()
-  create(@Body() createRoomDto: CreateRoomDto) {
-    return this.roomService.create(createRoomDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.roomService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roomService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
-    return this.roomService.update(+id, updateRoomDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roomService.remove(+id);
+  createRoomAndUserHost(@Body() createRoomDto: CreateRoomDto) {
+    return this.roomService.createRoomAndUserHost(createRoomDto);
   }
 }
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RoomController],
-  providers: [RoomService]
+  providers: [RoomService, UserService]
 })
 export class RoomModule {}
