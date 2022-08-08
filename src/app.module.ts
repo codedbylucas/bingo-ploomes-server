e3ed80d8169c6159e0e8fc92da1a0e6b0f1e16c0
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-08 19:54:45 -0300

    feat: resource user and room

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RoomModule } from './room/room.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, RoomModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
