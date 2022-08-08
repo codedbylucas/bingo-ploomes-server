6a9f40f8613fef45f27919f31d450cb1d87d7160
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-08 19:55:22 -0300

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000)
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Bingo Ploomes')
+    .setDescription('Bingo Ploomes')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('room')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3333);
 }
 bootstrap();
