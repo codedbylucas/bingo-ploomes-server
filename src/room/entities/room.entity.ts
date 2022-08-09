87895b743603f5313514a64eaa782d4fa388a8d5
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-09 17:27:04 -0300

    feat(room): create room entity

--- src/room/entities/room.entity.ts
+++ src/room/entities/room.entity.ts
@@ -1,1 +1,9 @@
-export class Room {}
+import { User } from 'src/user/entities/user.entity';
+
+export class Room {
+  id: string;
+  name: string;
+  drawnNumbers: number[];
+  status: boolean;
+  users: User;
+}
