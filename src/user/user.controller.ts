cdc3d4d8d346bec6199dce21ba9497b35f053373
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-08 21:57:37 -0300

    feat(user): implement return type to createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,8 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
 
 @ApiTags('user')
 @Controller('user')
@@ -18,7 +10,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,12 +3,13 @@ import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
     try {
       const data: Prisma.UserCreateInput = {
         nickname: createUserDto.nickname,
@@ -20,7 +21,15 @@ export class UserService {
         },
       };
 
-      return await this.prisma.user.create({ data });
+      return await this.prisma.user.create({
+        data,
+        select: {
+          id: true,
+          roomId: true,
+          nickname: true,
+          score: true,
+        },
+      });
     } catch (error) {
       console.log(error);
     }
