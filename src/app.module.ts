09ba1908a331f470cf4f60395004481219ed07f7
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-09 23:48:18 -0300

    feat: resource card

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { RoomModule } from './room/room.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CardModule } from './card/card.module';
 
 @Module({
-  imports: [UserModule, RoomModule, PrismaModule],
+  imports: [UserModule, RoomModule, PrismaModule, CardModule],
   controllers: [AppController],
   providers: [AppService],
 })
