1129b4d01eda27f117102a6c2689975fa0b9ab33
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-10 17:52:26 -0300

    fix(card): change card numbers

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -17,7 +17,10 @@ export class CardService {
 
   async createCard(createCardDto: CreateCardDto): Promise<Card> {
     const data: Prisma.CardCreateInput = {
-      numbers: [1, 2, 3],
+      numbers: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25,
+      ],
       user: {
         connect: {
           id: createCardDto.userId,
