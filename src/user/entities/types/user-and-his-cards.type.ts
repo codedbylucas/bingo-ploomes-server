b25ac16ec82a18d618c7c0d808a92b3145009b49
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-10 21:12:18 -0300

    feat(user): change in card field type

--- src/user/entities/types/user-and-his-cards.type.ts
+++ src/user/entities/types/user-and-his-cards.type.ts
@@ -1,8 +1,8 @@
-import { AssembledCard } from 'src/card/entities/types/assembled-card.type';
+import { Card } from 'src/card/entities/card.entity';
 
 export type UserAndHisCards = {
   id: string;
   nickname: string;
   score: number;
-  cards: AssembledCard[];
+  cards: Card[];
 };
