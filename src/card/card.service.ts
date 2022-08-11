041d39b5d52a0b134e0f07ab7f45506b243905af
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-08-11 16:21:34 -0300

    feat(card): implement `cardsGenerator`

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -5,6 +5,7 @@ import { UserService } from 'src/user/user.service';
 import { serverError } from 'src/utils/server-error.util';
 import { CreateCardDto } from './dto/create-card.dto';
 import { Card } from './entities/card.entity';
+import { GeneratedCard } from './entities/types/generated-card.type';
 
 @Injectable()
 export class CardService {
@@ -16,23 +17,22 @@ export class CardService {
   ) {}
 
   async createCard(createCardDto: CreateCardDto): Promise<Card[]> {
-    const data: Prisma.CardCreateInput = {
-      numbers: {
-        B: [8, 2, 12, 7, 10],
-        I: [29, 23, 20, 17, 22],
-        N: [38, 31, 43, 43, 35],
-        G: [46, 46, 52, 50, 56],
-        O: [64, 71, 68, 66, 68],
-      },
-      user: {
-        connect: {
-          id: createCardDto.userId,
-        },
-      },
-    };
+    const generatedCards: GeneratedCard[] = this.cardsGenerator(
+      createCardDto.userCards,
+    );
 
-    const cards = [];
+    const cards: Card[] = [];
+
     for (let i = 0; i < createCardDto.userCards; i++) {
+      const data: Prisma.CardCreateInput = {
+        numbers: generatedCards[i],
+        user: {
+          connect: {
+            id: createCardDto.userId,
+          },
+        },
+      };
+
       const card: Card = await this.prisma.card
         .create({
           data,
@@ -45,59 +45,95 @@ export class CardService {
 
       cards.push(card);
     }
-
     return cards;
   }
-}
 
-let colB: number[] = [];
-let colI: number[] = [];
-let colN: number[] = [];
-let colG: number[] = [];
-let colO: number[] = [];
+  cardsGenerator(numberOfUserCards: number): GeneratedCard[] {
+    const generatedCards: GeneratedCard[] = [];
 
-let card: unknown = {
-  B: colB,
-  I: colI,
-  N: colN,
-  G: colG,
-  O: colO,
-};
-
-function cardGenerator(
-  colLetter: number[],
-  max: number,
-  min: number,
-): number[] {
-  while (colLetter.length < 5) {
-    const randomNums = Math.floor(Math.random() * (max - min)) + min;
-    if (!colLetter.includes(randomNums)) {
-      colLetter.push(randomNums);
-    }
-  }
-  return colLetter;
-}
-
-function userCard(numberOfUserCards: number): void {
-  for (let i = 0; i < numberOfUserCards; i++) {
-    cardGenerator(colB, 16, 1);
-    cardGenerator(colI, 31, 16);
-    cardGenerator(colN, 46, 31);
-    cardGenerator(colG, 61, 46);
-    cardGenerator(colO, 76, 61);
-    colB = [];
-    colI = [];
-    colN = [];
-    colG = [];
-    colO = [];
-    console.log(card);
-    card = {
-      B: colB,
-      I: colI,
-      N: colN,
-      G: colG,
-      O: colO,
+    let card: GeneratedCard = {
+      B: [],
+      I: [],
+      N: [],
+      G: [],
+      O: [],
     };
+
+    const cardGenerator = (
+      colLetter: number[],
+      max: number,
+      min: number,
+    ): number[] => {
+      while (colLetter.length < 5) {
+        const randomNums: number =
+          Math.floor(Math.random() * (max - min)) + min;
+        if (!colLetter.includes(randomNums)) {
+          colLetter.push(randomNums);
+        }
+      }
+      return colLetter;
+    };
+
+    for (let i = 0; i < numberOfUserCards; i++) {
+      cardGenerator(card.B, 16, 1);
+      cardGenerator(card.I, 31, 16);
+      cardGenerator(card.N, 46, 31);
+      cardGenerator(card.G, 61, 46);
+      cardGenerator(card.O, 76, 61);
+
+      generatedCards.push(card);
+      card = {
+        B: [],
+        I: [],
+        N: [],
+        G: [],
+        O: [],
+      };
+    }
+
+    return generatedCards;
   }
 }
-userCard(3);
+
+// let colB: number[] = [];
+// let colI: number[] = [];
+// let colN: number[] = [];
+// let colG: number[] = [];
+// let colO: number[] = [];
+
+// let card = {
+//   B: [],
+//   I: [],
+//   N: [],
+//   G: [],
+//   O: [],
+// };
+
+// function cardGenerator(colLetter: any, max: number, min: number): number[] {
+//   while (colLetter.length < 5) {
+//     const randomNums = Math.floor(Math.random() * (max - min)) + min;
+//     if (!colLetter.includes(randomNums)) {
+//       colLetter.push(randomNums);
+//     }
+//   }
+//   return colLetter;
+// }
+
+// function userCard(numberOfUserCards: number): void {
+//   for (let i = 0; i < numberOfUserCards; i++) {
+//     cardGenerator(card.B, 16, 1);
+//     cardGenerator(card.I, 31, 16);
+//     cardGenerator(card.N, 46, 31);
+//     cardGenerator(card.G, 61, 46);
+//     cardGenerator(card.O, 76, 61);
+//     console.log(card);
+//     card = {
+//       B: [],
+//       I: [],
+//       N: [],
+//       G: [],
+//       O: [],
+//     };
+//   }
+// }
+// userCard(3);
