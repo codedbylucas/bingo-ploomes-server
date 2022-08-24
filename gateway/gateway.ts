aacb52b34426e4868970f21360382bd7dd67d3cd
DJMarshallSantos <deejaymarshall@msn.com>
2022-08-23 21:32:39 -0400

    Update gateway.ts

--- gateway/gateway.ts
+++ gateway/gateway.ts
@@ -1,4 +1,39 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
-export class AppGateway {}
+@WebSocketGateway({ namespace: '/chat' })
+export class AppGateway implements OnGatewayInit {
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(
+    client: Socket,
+    message: { sender: string; room: string; message: string },
+  ) {
+    this.wss.to(message.room).emit('chatToClient', message);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleRoomJoin(client: Socket, room: string) {
+    client.join(room);
+    client.emit('joinedRoom', room);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleRoomLeave(client: Socket, room: string) {
+    client.leave(room);
+    client.emit('leftRoom', room);
+  }
+}
